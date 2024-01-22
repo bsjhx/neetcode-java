dac6cc8b45216c8635e907c603cecf383d359944
bsjhx <rakoczymichal@hotmail.com>
2024-01-22 22:28:06 +0100

    226. Invert Binary Tree, 104. Maximum Depth of Binary Tree

---
+++ src/main/java/pl/bsjhx/trees/InvertBinaryTree.java
@@ -0,0 +1,24 @@
+package pl.bsjhx.trees;
+
+class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left != null) {
+            invertTree(root.left);
+        }
+        if (root.right != null) {
+            invertTree(root.right);
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        return root;
+    }
+}
