8668d3fa31823da3302bcdf3c514e5b7ec090398
bsjhx <rakoczymichal@hotmail.com>
2024-01-24 10:29:11 +0100

    572. Subtree of Another Tree

--- src/main/java/pl/bsjhx/trees/SameTree.java
+++ src/main/java/pl/bsjhx/trees/SameTree.java
@@ -14,4 +14,11 @@ class SameTree {
 
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null || isSameTree(root, subRoot)) return true;
+        if (root == null) return false;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
 }
