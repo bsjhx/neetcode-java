66076d45c09f74a91df582b940c634498b7a357b
bsjhx <rakoczymichal@hotmail.com>
2024-01-31 11:00:47 +0100

    191. Number of 1 Bits

--- src/main/java/pl/bsjhx/bitmanipulation/SingleNumber.java
+++ src/main/java/pl/bsjhx/bitmanipulation/SingleNumber.java
@@ -9,4 +9,18 @@ class SingleNumber {
 
         return result;
     }
+
+    public int hammingWeight(int n) {
+        if (n < 0) {
+            n = -n;
+        }
+        int r = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                r++;
+            }
+            n = n >> 1;
+        }
+        return r;
+    }
 }
