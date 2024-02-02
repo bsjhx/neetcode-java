ddd251d5ec352f340cbbf33a50a767d2e8423362
bsjhx <rakoczymichal@hotmail.com>
2024-02-02 13:55:41 +0100

    Meeting Schedule, Meetings Room

---
+++ src/main/java/pl/bsjhx/intervals/Interval.java
@@ -0,0 +1,9 @@
+package pl.bsjhx.intervals;
+
+class Interval {
+     public int start, end;
+     public Interval(int start, int end) {
+         this.start = start;
+         this.end = end;
+     }
+ }
