d7685a4c3da526ad54e65c1a7ce3068d67fb19e8
bsjhx <rakoczymichal@hotmail.com>
2024-02-03 21:54:42 +0100

    167. Two Sum II - Input Array Is Sorted

---
+++ src/test/java/pl/bsjhx/twopointers/TwoSumTest.java
@@ -0,0 +1,17 @@
+package pl.bsjhx.twopointers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    void twoSum() {
+        TwoSum ts = new TwoSum();
+        int[] res = ts.twoSum(new int[]{-10,-8,-2,1,2,5,6}, 0);
+        System.out.println(Arrays.toString(res));
+    }
+}
