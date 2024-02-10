8ca77b17de077ae2e85b68b4b0ee7f0fc3ecb74b
bsjhx <rakoczymichal@hotmail.com>
2024-02-10 23:18:57 +0100

    268. Missing Number

--- src/main/java/pl/bsjhx/bitmanipulation/BitsManipulationsEasy.java
+++ src/main/java/pl/bsjhx/bitmanipulation/BitsManipulationsEasy.java
@@ -37,4 +37,13 @@ class BitsManipulationsEasy {
 
         return res;
     }
+
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int total = nums.length * (nums.length + 1) / 2;
+        for (int num : nums) {
+            sum += num;
+        }
+        return total - sum;
+    }
 }
