340ded34f4f6477f032cf0667a5fdd808698cf17
bsjhx <rakoczymichal@hotmail.com>
2024-02-13 16:38:33 +0100

    739. Daily Temperatures

---
+++ src/test/java/pl/bsjhx/stack/DailyTemperaturesTest.java
@@ -0,0 +1,20 @@
+package pl.bsjhx.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DailyTemperaturesTest {
+
+    @Test
+    void dailyTemperatures() {
+        DailyTemperatures dt = new DailyTemperatures();
+        int[] ints = dt.dailyTemperatures(new int[]{30, 40, 50, 60});
+        System.out.println(Arrays.toString(ints));
+
+        ints = dt.dailyTemperatures(new int[]{89,62,70,58,47,47,46,76,100,70});
+        System.out.println(Arrays.toString(ints));
+    }
+}
